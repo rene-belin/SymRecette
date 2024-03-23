f319b83d1a89d7b303482b56149cfcdc81b3f3e0
rene-belin <rene.b@gmx.fr>
2024-03-23 20:50:22 +0100

    Sécurité User & Hachage de mot de passe

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,10 +3,11 @@
 // src/DataFixtures/AppFixtures.php
 namespace App\DataFixtures;
 
-use App\Entity\Ingredient;
+use App\Entity\User;
 use App\Entity\Recipe;
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Ingredient;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
@@ -17,7 +18,7 @@ class AppFixtures extends Fixture
         for ($i = 0; $i < 50; ++$i) {
             $ingredient = new Ingredient();
             $ingredient->setName('Ingredient ' . ($i + 1))
-                       ->setPrice(mt_rand(0, 100));
+                ->setPrice(mt_rand(0, 100));
             $ingredients[] = $ingredient;
             $manager->persist($ingredient);
         }
@@ -26,18 +27,40 @@ class AppFixtures extends Fixture
         for ($i = 0; $i < 25; $i++) {
             $recipe = new Recipe();
             $recipe->setName('Recipe ' . ($i + 1))
-                   ->setTime(rand(10, 120))
-                   ->setNbPeople(rand(1, 6))
-                   ->setDifficulty(rand(1, 3))
-                   ->setDescription('This is a sample description for recipe ' . ($i + 1))
-                   ->setPrice(rand(500, 2000) / 100)
-                   ->setIsFavorite(rand(0, 1) == 1)
-                   ->setCreatedAt(new \DateTimeImmutable())
-                   ->setUpdatedAt(new \DateTimeImmutable());
+                ->setTime(rand(10, 120))
+                ->setNbPeople(rand(1, 6))
+                ->setDifficulty(rand(1, 3))
+                ->setDescription('This is a sample description for recipe ' . ($i + 1))
+                ->setPrice(rand(500, 2000) / 100)
+                ->setIsFavorite(rand(0, 1) == 1)
+                ->setCreatedAt(new \DateTimeImmutable())
+                ->setUpdatedAt(new \DateTimeImmutable());
 
             $manager->persist($recipe);
         }
 
+        // Admin User
+        $admin = new User();
+        $admin->setFullName('Administrateur de SymRecipe')
+            ->setPseudo(null)
+            ->setEmail('admin@symrecipe.fr')
+            ->setRoles(['ROLE_USER', 'ROLE_ADMIN'])
+            ->setPlainPassword('password'); // Définissez simplement le mot de passe en clair
+
+        $manager->persist($admin);
+
+        // Regular Users
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setFullName('User' . ($i + 1))
+                ->setPseudo(mt_rand(0, 1) === 1 ? 'Pseudo' . ($i + 1) : null)
+                ->setEmail('user' . ($i + 1) . '@example.com')
+                ->setRoles(['ROLE_USER'])
+                // Définissez également le mot de passe en clair pour les utilisateurs réguliers
+                ->setPlainPassword('password');
+
+            $manager->persist($user);
+        }
         $manager->flush();
     }
 }
---
+++ src/EntityListener/UserListener.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\EntityListener;
+
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+/**
+ * La classe UserListener est responsable de l'encodage du mot de passe d'une
+ * entité User avant qu'elle ne soit enregistrée ou mise à jour dans la base de
+ * données.
+ */
+class UserListener
+{
+    private UserPasswordHasherInterface $hasher;
+
+    /**
+     * Constructeur de la classe UserListener.
+     *
+     * @param UserPasswordHasherInterface $hasher Le service de hachage de mot de passe.
+     */
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    /**
+     * Écouteur d'événement prePersist.
+     *
+     * Cette méthode est appelée avant qu'une entité User soit persistée.
+     * Elle encode le mot de passe de l'utilisateur.
+     *
+     * @param User $user L'entité User qui va être persistée.
+     */
+    public function prePersist(User $user)
+    {
+        $this->encodePassword($user);
+    }
+
+    /**
+     * Écouteur d'événement preUpdate.
+     *
+     * Cette méthode est appelée avant qu'une entité User soit mise à jour.
+     * Elle encode le mot de passe de l'utilisateur s'il a été modifié.
+     *
+     * @param User $user L'entité User qui va être mise à jour.
+     */
+    public function preUpdate(User $user)
+    {
+        $this->encodePassword($user);
+    }
+
+    /**
+     * Encode le mot de passe en se basant sur le mot de passe en clair.
+     *
+     * Si le mot de passe en clair est null, la méthode ne fait rien.
+     * Sinon, elle encode le mot de passe et le définit dans l'entité User,
+     * puis efface le mot de passe en clair pour des raisons de sécurité.
+     *
+     * @param User $user L'entité User dont le mot de passe doit être encodé.
+     * @return void
+     */
+    public function encodePassword(User $user)
+    {
+        if ($user->getPlainPassword() === null) {
+            return;
+        }
+
+        $user->setPassword(
+            $this->hasher->hashPassword(
+                $user,
+                $user->getPlainPassword()
+            )
+        );
+
+        // Efface le mot de passe en clair après l'encodage pour la sécurité.
+        $user->setPlainPassword(null);
+    }
+}
