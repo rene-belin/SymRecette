b4840661728824f0dff3cddfad769b57d41356eb
rene-belin <rene.b@gmx.fr>
2024-03-23 23:09:38 +0100

    Sécurité Firewall & Formulaire de connexion

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    // login
+    #[Route('/connexion', name: 'security.login', methods: ['GET', 'POST'])]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        return $this->render('pages/security/login.html.twig', [
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error'=>$authenticationUtils->getLastAuthenticationError()
+        ]);
+    }
+
+    // logout
+    #[Route('/deconnexion', 'security.logout')]
+    public function logout()
+    {
+        // nothing to do here...
+    }
+}
--- src/Form/IngredientType.php
+++ src/Form/IngredientType.php
@@ -17,42 +17,51 @@ class IngredientType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            // Ajout du champ 'name' de type texte
             ->add('name', TextType::class, [
+                // Attributs HTML pour le champ 'name'
                 'attr' => [
-                    'class' => 'form-control',
-                    'minlength' => '2',
-                    'maxlength' => '50'
+                    'class' => 'form-control', // Classe CSS pour le style
+                    'minlength' => '2',        // Longueur minimale du texte
+                    'maxlength' => '50'        // Longueur maximale du texte
                 ],
-                'label' => 'Nom',
+                'label' => 'Nom', // Label du champ
+                // Attributs HTML pour le label du champ 'name'
                 'label_attr' => [
-                    'class' => 'form-label mt-4'
+                    'class' => 'form-label mt-4' // Classe CSS pour le style du label
                 ],
+                // Contraintes de validation pour le champ 'name'
                 'constraints' => [
-                    new Assert\Length(['min' => 2, 'max' => 50]),
-                    new Assert\NotBlank()
+                    new Assert\Length(['min' => 2, 'max' => 50]), // Longueur du texte
+                    new Assert\NotBlank() // Le champ ne doit pas être vide
                 ]
             ])
+            // Ajout du champ 'price' de type monétaire
             ->add('price', MoneyType::class, [
+                // Attributs HTML pour le champ 'price'
                 'attr' => [
-                    'class' => 'form-control',
+                    'class' => 'form-control', // Classe CSS pour le style
                 ],
-                'label' => 'Prix ',
+                'label' => 'Prix ', // Label du champ
+                // Attributs HTML pour le label du champ 'price'
                 'label_attr' => [
-                    'class' => 'form-label mt-4'
+                    'class' => 'form-label mt-4' // Classe CSS pour le style du label
                 ],
+                // Contraintes de validation pour le champ 'price'
                 'constraints' => [
-                    new Assert\Positive(),
-                    new Assert\LessThan(200)
+                    new Assert\Positive(), // Le prix doit être positif
+                    new Assert\LessThan(200) // Le prix doit être inférieur à 200
                 ]
             ])
+            // Ajout d'un bouton de soumission
             ->add('submit', SubmitType::class, [
+                // Attributs HTML pour le bouton de soumission
                 'attr' => [
-                    'class' => 'btn btn-primary mt-4'
+                    'class' => 'btn btn-primary mt-4' // Classe CSS pour le style
                 ],
-                'label' => 'Créer mon ingrédient'
+                'label' => 'Créer mon ingrédient' // Texte du bouton
             ]);
     }
-
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
